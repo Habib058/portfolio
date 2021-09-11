fc330cc1642699bab8f4d87ffc8f769454cfe338
Habib058 <adnanshakil215@gmail.com>
2021-09-11 19:21:27 +0600

    footer done

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import facebook from '../../assets/images/facebook.png'
+import lninkedIn from '../../assets/images/linkedin.png'
+import git from '../../assets/images/git.jpg'
+import './Footer.css'
 
 const Footer = () => {
     return (
@@ -11,21 +15,24 @@ const Footer = () => {
                     <p>Web Developer | App Developer</p>
                 </FooterDesc>
                 <FooterLInkItems>
-                    <FooterLinkTitle>Contact me</FooterLinkTitle>
-                    <FooterLink to="">contact</FooterLink>
-                    <FooterLink to="">contact</FooterLink>
+                    <FooterLinkTitle>Skills</FooterLinkTitle>
+                    <FooterLink to="">React JS</FooterLink>
+                    <FooterLink to="">Node JS</FooterLink>
+                    <FooterLink to="">Mongo DB</FooterLink>
                 </FooterLInkItems>
             </FooterLinksWrapper>
             <FooterLinksWrapper>
                 <FooterLInkItems>
                     <FooterLinkTitle>Projects</FooterLinkTitle>
-                    <FooterLink to="">contact</FooterLink>
-                    <FooterLink to="">contact</FooterLink>
+                    <FooterLink href="https://clone-058.web.app/" target="_blank">Amazon Clone</FooterLink>
+                    <FooterLink href="https://travio-058.web.app/" target="_blank">Travio</FooterLink>
+                    <FooterLink href="https://food-to-fly-347a7.web.app/" target="_blank">Food-To-Fly</FooterLink>
                 </FooterLInkItems>
                 <FooterLInkItems>
                     <FooterLinkTitle>Social Media</FooterLinkTitle>
-                    <FooterLink to="">contact</FooterLink>
-                    <FooterLink to="">contact</FooterLink>
+                    <FooterLink href="https://www.facebook.com/Habib058/" target="_blank"><img className="logo" src={facebook} alt="" /></FooterLink>
+                    <FooterLink href="https://www.linkedin.com/in/habib-shakil-a8a703203/" target="_blank"><img className="logo" src={lninkedIn} alt="" /></FooterLink>
+                    <FooterLink href="https://github.com/Habib058" target="_blank"><img className="logo" src={git} alt="" /></FooterLink>
                 </FooterLInkItems>
             </FooterLinksWrapper>
         </FooterContainer>
@@ -44,7 +51,7 @@ background: black;
 const FooterDesc = styled.div`
 padding: 0 2rem;
 h1{
-    margin-bottom: 1rem;
+    margin-bottom: 2rem;
     color: #f26a2e;
 }
 @media screen and (max-width: 400px) {
@@ -72,13 +79,13 @@ padding: 1rem 2rem;
 
 `
 const FooterLinkTitle = styled.h2`
-font-size: 14px;
+font-size: 20px;
 margin-bottom: 16px;
 `
-const FooterLink = styled(Link)`
+const FooterLink = styled.a`
 text-decoration: none;
-margin-bottom: 0%.5rem;
-font-size: 14px;
+margin-bottom: 0.5rem;
+font-size: 20px;
 color: white;
 
 &:hover {
